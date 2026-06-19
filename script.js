dd8c79dc0b7c7a5b5d50437555ea60c67b072d8e
NamanVaish <namanvaish53@gmail.com>
2026-06-19 23:54:04 +0530

    page-3 completed

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true
+});
+
+let elemContainer = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+
+elemContainer.addEventListener("mouseenter", () => {
+    fixedImage.style.display = "block";
+});
+
+elemContainer.addEventListener("mouseleave", () => {
+    fixedImage.style.display = "none";
+});
+
+let elems = document.querySelectorAll(".elem");
+elems.forEach(e => {
+    e.addEventListener("mouseenter", () => {
+        let image = e.getAttribute("data-image");
+        fixedImage.style.backgroundImage = `url(${image})`;
+    });
 });
